49255f77dc3f961095205962acba66c74f63bad3
Andrew Thomas <andrewmthomas87@gmail.com>
2016-01-17 22:41:28 -0700

    feat(draft): Draft order generating added

--- socket/admin.js
+++ socket/admin.js
@@ -3,22 +3,99 @@ var connection = require('../database')
 
 var SocketHandler = require('./SocketHandler')
 
+var admins
+
+connection.query('select id from users where admin=1', function(error, rows) {
+	if (error) {
+		rows = []
+	}
+
+	admins = rows.map(function(row) {
+		return row.id
+	})
+})
+
+function isAdmin(id) {
+	return admins.indexOf(id) > -1
+}
+
 function admin(socket) {
 	var adminSocketHandler = SocketHandler(socket, 'Admin')
 
 	adminSocketHandler.register('verify', function() {
-		connection.query('select admin from users where id=?', [socket.decodedToken.id], function(error, rows) {
+		if (isAdmin(socket.decodedToken.id)) {
+			adminSocketHandler.send('verify', 'Access granted')
+		}
+		else {
+			adminSocketHandler.error('verify', 'Access denied')
+		}
+	})
+
+	adminSocketHandler.register('generateDraft', function() {
+		if (!isAdmin(socket.decodedToken.id)) {
+			adminSocketHandler.error('generateDraft', 'Access denied')
+			return
+		}
+
+		connection.query('select count(*) generated from draftOrder', function(error, rows) {
 			if (error) {
-				adminSocketHandler.error('verify', 'Server error')
+				adminSocketHandler.error('generateDraft', 'Server error')
 				return
 			}
 
-			if (rows.length && rows[0].admin) {
-				adminSocketHandler.send('verify', 'Access granted')
+			if (rows[0].generated) {
+				adminSocketHandler.send('generateDraft', false)
+				return
 			}
-			else {
-				adminSocketHandler.error('verify', 'Access denied')
+
+			connection.query('select id from users', function(error, rows) {
+				if (error) {
+					adminSocketHandler.error('generateDraft', 'Server error')
+					return
+				}
+
+				var userIDs = rows.map(function(row) {
+					return row.id
+				})
+
+				for (var i = 0; i < 42; ++i) {
+					userIDs.sort(function(a, b) {
+						return Number(Math.random() >= 0.5) * 2 - 1
+					})
+				}
+
+				userIDs = userIDs.map(function(id, position) {
+					return [
+						position,
+						id
+					]
+				})
+
+				connection.query('insert into draftOrder (position, id) values ?', [userIDs], function(error) {
+					if (error) {
+						adminSocketHandler.error('generateDraft', 'Server error')
+						return
+					}
+
+					adminSocketHandler.send('generateDraft', true)
+				})
+			})
+		})
+	})
+
+	adminSocketHandler.register('getDraftOrder', function() {
+		if (!isAdmin(socket.decodedToken.id)) {
+			adminSocketHandler.error('Access denied')
+			return
+		}
+
+		connection.query('select a.id, concat(firstName, " ", lastName) name, b.position, active from users a, draftOrder b where a.id=b.id order by position', function(error, rows) {
+			if (error) {
+				adminSocketHandler.error('getDraftOrder', 'Server error')
+				return
 			}
+
+			adminSocketHandler.send('getDraftOrder', rows)
 		})
 	})
 }
